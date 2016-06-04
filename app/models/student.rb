a77446c74871e8ba44c8af7cf7491952ad2a8739
reina <angelhyl@hotmail.com>
2016-06-04 23:52:50 +0000

    added validation of text field

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,6 @@
 class Student < ActiveRecord::Base
-    
+    validates :name, :weight, :height, :color, :gpa, :email, presence: true 
+    validates_numericality_of :height, :weight, :gpa
+
+                    
 end
